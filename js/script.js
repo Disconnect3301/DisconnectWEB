670490e4089f2ee8c97a5e012628bdbc6218e686
Disconnect3301 <disc0nnect@list.ru>
2024-08-16 22:02:09 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// const doodle = document.querySelector('.doodle');
+
+// function updateDoodlePosition() {
+//   doodle.style.top = `${window.scrollY + window.innerHeight / 2}px`;
+//   doodle.style.left = `${window.scrollX + window.innerWidth / 2}px`;
+// }
+
+// window.addEventListener('scroll', updateDoodlePosition);
+// window.addEventListener('load', updateDoodlePosition);
